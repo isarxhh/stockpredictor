3ced994304fede55abf1c5254b0116c1936f5111
SAYANTANI <sayantanipatra089@outlook.com>
2025-12-04 20:56:37 +0530

    final commit

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown(
     "<div class='header'>AI Stock Prediction Dashboard</div>", unsafe_allow_html=True
 )
 st.markdown(
-    "<div class='subtle'>Yahoo Finance + simple LSTM — small demo (not financial advice)</div>",
+    "<div class='subtle'>Yahoo Finance + simple LSTM</div>",
     unsafe_allow_html=True,
 )
 
